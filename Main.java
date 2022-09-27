8da8ffe326635cc8ee65888627fcabb147ee2b22
joaofrigo <92951251+joaofrigo@users.noreply.github.com>
2022-09-27 12:26:05 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Jureg");
+	}
+	Shrek c;
+	int a;
+	float b;
+	c = new Shrek();
+}
+
+/*
+
+    A classe deve conter atributos de pelo menos 2 tipos diferentes (algumas possibilidades: String, int, double, boolean, etc.).
+
+    A classe deve conter um construtor (pode haver mais de um, com diferentes argumentos).
+
+    A classe deve conter métodos com prefixo get e set, respectivamente para obter e alterar cada atributo.
+
+    A classe deve conter um método que implemente alguma operação com um ou mais atributos (por exemplo, um teste de condição, um cálculo, uma operação com strings, etc.).
+*/
